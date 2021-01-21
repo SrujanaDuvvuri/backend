1fa7538de69bba85616b28801601006c5a00adb4
SrujanaDuvvuri <suji.duvvuri@gmail.com>
2021-01-22 03:24:38 +0530

    backend all files

---
+++ oopsproject/src/oopsproject/SavingsAccount.java
@@ -0,0 +1,72 @@
+package oopsproject;
+
+//base class or parent class or base class
+public class SavingsAccount {
+
+	
+	int accountNo;	
+	float balance;
+	int pin;
+	final static int minBalance = 500;
+	
+	public static int getMinBalance() {
+		return minBalance;
+	}
+//	public static void setMinBalance(int minBalance) {
+//		SavingsAccount.minBalance = minBalance;
+//	}
+	
+	
+	//default constructor
+	public SavingsAccount() {
+		
+	}
+	//parameterised constructor 
+							// parameters
+	public SavingsAccount(int accNo, float bal, int pin) {
+		this.accountNo = accNo;
+		this.balance = bal;
+		this.pin = pin;
+	}
+	protected void testProtected() {
+		
+	}
+	public void deposit(int depositAmt) {
+		if(depositAmt>0) {
+			balance = balance + depositAmt;
+		}else {
+			System.out.println("Invalid amt");
+		}
+	}
+	
+	//withdraw(int)
+	public void withdraw(int withdrawalAmt) {
+		if(withdrawalAmt <= balance)
+		{
+			balance = balance - withdrawalAmt;
+		}else {
+			System.out.println("Insufficient funds");
+		}
+	}
+	
+	//withdraw(int,int)
+	public void withdraw(int pin, int withdrawalAmt) {
+		if(this.pin == pin)
+		{
+			if(withdrawalAmt <=4000)
+			{
+				withdraw(withdrawalAmt);
+			}else {
+				System.out.println("withdrawal lint exceeded");
+			}
+		}else {
+			System.out.println("invalid pin");
+		}
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "SavingsAccount [accountNo=" + accountNo + ", balance=" + balance + ", pin=" + pin + "]";
+	}	
+}
